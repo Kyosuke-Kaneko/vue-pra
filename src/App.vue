29f1d698f0c782991eb657992101237ceee7effb
Kyosuke Kaneko <kyosuke.soccer118@gmail.com>
2021-01-22 13:10:56 +0900

    table-pra

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,60 @@
 <template>
- <div id="app">
-   <h1>Hello world</h1>
- </div>
+  <div id="app">
+    <table>
+      <tr>
+        <th>名前</th>
+        <th>値段</th>
+        <th>個数</th>
+      </tr>
+      <tr>
+        <td>{{pencil.name}}</td>
+        <td>{{pencil.price}}</td>
+        <td>{{pencil.quantity}}</td>
+      </tr>
+      <tr>
+        <td>{{eraser.name}}</td>
+        <td>{{eraser.price}}</td>
+        <td>{{eraser.quantity}}</td>
+      </tr>
+      <tr>
+        <td>{{notebook.name}}</td>
+        <td>{{notebook.price}}</td>
+        <td>{{notebook.quantity}}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
-<script></script>
-
+<script>
+export default {
+  data() {
+    return {
+      pencil:{
+        name: "鉛筆",
+        price: 100,
+        quantity: 0
+      },
+      eraser:{
+        name:'消しゴム',
+        price:150,
+        quantity:0
+      },
+      notebook:{
+        name:'ノート',
+        price:200,
+        quantity:0
+      }
+    };
+  }
+};
+</script>
 <style>
- #app{
-   color: red;
- }
+th {
+  width: 30%;
+  background: #e6e6e6;
+}
+td {
+  width: 30%;
+  background: #f2f2f2;
+}
 </style>
