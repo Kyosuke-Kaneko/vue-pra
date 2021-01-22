710a0388f5141691c9dfd278c0f508c27686b1f2
Kyosuke Kaneko <kyosuke.soccer118@gmail.com>
2021-01-22 14:39:15 +0900

    双方向バインディング

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <template>
   <div id="app">
-    <table>
-      <tr v-for="(data,index) in Vegetables" :key="index">
-        <th>{{index}}</th>
-        <td>{{data.name}}</td>
-        <td>{{data.price}}円</td>
-      </tr>
-    </table>
+    <input type="text" v-model="text" />
+    <p>{{text}}</p>
   </div>
 </template>
 
@@ -14,11 +9,7 @@
 export default {
   data() {
     return {
-      Vegetables: [
-        { name: "じゃがいも", price: 300 },
-        { name: "イチゴ", price: 400 },
-        { name: "カボチャ", price: 500 }
-      ]
+      text: ""
     };
   }
 };
