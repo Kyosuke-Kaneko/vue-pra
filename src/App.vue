b064d94d3fe3a750ba8bac112cf78aca72e7e6e7
Kyosuke Kaneko <kyosuke.soccer118@gmail.com>
2021-01-22 20:27:40 +0900

    par=>child

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,20 @@
 <template>
   <div id="app">
-    名前：
-    <input type="text" v-model="name" />
-    値段：
-    <input type="number" v-model="price" />
-    <button @click="add">果物</button>
-     <ul>
-      <li v-for="(item,index) in list" :key="index">{{ item.name }}は{{ item.price }}円</li>
-    </ul>
+    <Header :parentText="sendText"></Header>
+    <p>App.vue</p>
   </div>
 </template>
 
 <script>
+import Header from "./components/Header";
 export default {
   data() {
     return {
-      name: "",
-      price: 0,
-      list: [
-        { name: "りんご", price: 200 },
-        { name: "オレンジ", price: 100 },
-        { name: "バナナ", price: 500 }
-      ]
+      sendText: "親コンポーネントのデータ"
     };
   },
-  methods: {
-    add() {
-      this.list.push({
-        name: this.name,
-        price: this.price
-      });
-    }
+  components: {
+    Header
   }
 };
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="header">
+    <p>{{parentText}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["parentText"]
+};
+</script>
